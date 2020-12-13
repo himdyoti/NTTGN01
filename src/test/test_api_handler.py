e981b6d10db64b8924c2ca0f8c92439085cba028
himdyoti <himdyoti@gmail.com>
2020-12-13 19:16:22 +0530

    test mocking data updated

--- src/test/test_api_handler.py
+++ src/test/test_api_handler.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 import sys
+from collections import OrderedDict
 sys.path.append("../")
 from ssh_dev_conn import parse_interface_data
 
@@ -8,7 +9,7 @@ from ssh_dev_conn import parse_interface_data
 with open("show_running-config.txt") as fd:
     cmd_outp = fd.read()
 with open("sample.json") as fd:
-    expected = json.load(fd)
+    expected = json.load(fd, object_pairs_hook=OrderedDict)
 
 
 class TestDevConn(unittest.TestCase):
@@ -16,6 +17,8 @@ class TestDevConn(unittest.TestCase):
     def test_parse_interface_data(self):
         parsed_data = parse_interface_data(cmd_outp)
         for expect, outpd in zip(expected, parsed_data):
+            if expect != outpd:
+                print(expect, outpd)
             assert expect == outpd
 
     def test_handler_get_interfaces(self):
